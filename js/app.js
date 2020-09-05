2d4180c8a38684ee4cad0dfeb92d1032eb07211c
Amr123w <amr123w@gmail.com>
2020-09-05 14:55:24 +0300

    hisham

--- js/app.js
+++ js/app.js
@@ -1,16 +1,55 @@
-let slides = document.querySelectorAll(".slide-items");
 
-let counter=0;
-let changeSlider = function () {
-    if(counter !== slides.length-1){
-        slides[counter].style.display="none";
-        slides[counter+1].style.display="block";
-        counter++;
-    }
-    else{
-        slides[counter].style.display="none";
-        slides[0].style.display="block";
-        counter=0;
+/////*/*//*/*/*/*/*/*/*///////////////
+
+
+let slides = document.querySelectorAll(".slide-item");
+let [arrow_right, arrow_left] = document.querySelectorAll(".arrow");
+let img = document.querySelectorAll(".images");
+
+slide(slides, 2000);
+slide(img, 2000);
+
+
+/////////////
+
+let cards = document.querySelectorAll(".card");
+for (let i = 0 ; i<cards.length; i++){
+
+    cards[i].onmouseover = function () {
+        for (let j =0; j< cards.length; j++) cards[j].classList.remove("active")
+        cards[i].classList.add("active");
     }
 }
-setInterval(changeSlider,1000);
+
+let btn = document.querySelectorAll(".select-button");
+let [x , y , z] = document.querySelectorAll(".feature-box");
+for (let i = 0 ; i<btn.length; i++){
+
+    btn[i].onclick = function () {
+        for (let j =0; j< btn.length; j++) btn[j].classList.remove("butt-active")
+        btn[i].classList.add("butt-active");
+        if (i === 0){
+            x.style.display = "flex";
+            y.style.display = "none";
+            z.style.display = "none";
+        }
+        if (i === 1){
+            y.style.display = "flex";
+            x.style.display = "none";
+            z.style.display = "none";
+        }
+        if (i === 2){
+            z.style.display = "flex";
+            x.style.display = "none";
+            y.style.display = "none";
+        }
+    }
+}
+
+/*let btn = document.querySelectorAll(".select-button");
+let boxes = document.getElementsByClassName()
+btn.forEach(function (btn, key) {
+    btn.onclick=function () {
+
+    }
+})*/
